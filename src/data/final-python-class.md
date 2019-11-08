b5288062794d1b1dde36a784f9beb78796d5710b
Muhammad Rusdi <muhammadrusdiid@gmail.com>
2019-11-08 00:58:10 +0000

    add final

---
+++ src/data/final-python-class.md
@@ -0,0 +1,23 @@
+---
+path: "final-python-class"
+date: "2019-05-04"
+title: "Final Python Class"
+---
+
+Pilihan Tugas akhir Intro Programming
+
+1. Tic Tac Toe Game
+
+   Buatlah game tic tac toe sederhana, dimana terdapat 2 pemain yang masing - masing akan menginput X dan O secara bergantian, permainan dinyatakan selesai jika terdapat deret X atau O yang tercipta (seperti gambar).
+
+
+![1*9LBDOx6gG4mTuPGPDp888w.png](https://miro.medium.com/max/512/1*9LBDOx6gG4mTuPGPDp888w.png)
+
+
+
+
+2. Buatlah calculator sederhana untuk operasi Penjumlahan (+), Pengurangan (-), Perkalian (x), dan Pembagian (/). Contoh seperti pada video, dimana setiap operasi memerlukan 2 buah inputan bilangan yang akan diproses.
+
+![movie](../images/scv.mov)
+
+![movie](../images/scv2.mov)
---
+++ src/pages/intro-programming-class/final-python-class.jsx
@@ -0,0 +1,79 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import {
+  Container,
+} from "../../components/styled";
+import Globalnav from "../../components/globalnav";
+import SEO from "../../components/gatsby-seo"
+import { css } from "@emotion/core"
+
+const Outline = () => {
+  const data = useStaticQuery(graphql`
+    query {
+    markdownRemark(frontmatter: { path: { eq: "final-python-class" } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+  `)
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+
+
+  return (
+    <div>
+    <SEO
+        title="Final Python Class - DTC Academy"
+        description="Be a skillful Programmer with Intro Programmer Class"
+      />
+      <Globalnav
+        title="Final Python Class"
+        top={0}
+        link="https://forms.gle/qiLvYUBfMZQRS7WYA"
+      />
+      <Container>
+        <div style={{
+            paddingTop: 140
+          }}
+
+          >
+        <h1>{frontmatter.title}</h1>
+        <div
+          className="blog-post-content"
+          css={css`
+            margin-top: 60px;
+            ol {
+              margin-bottom: 80px;
+            }
+            & > h2 {
+              font-size: 24px;
+              & + p {
+                margin-bottom: 30px;
+              }
+            }
+            ul {
+              & + h2 {
+                margin-top: 30px;
+              }
+            }
+            h3 {
+              font-size: 20px;
+              margin-top: 30px;
+            }
+            img {
+              width: 100%;
+            }
+          `}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Outline
